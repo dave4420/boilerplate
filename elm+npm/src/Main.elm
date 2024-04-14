2d696edfefc4e2cefe2d232a26b5116a0d1c1696
Dave Hinton <dave@4420.uk>
2024-04-14 11:32:33 +0100

    Hello, world!

---
+++ elm+npm/src/Main.elm
@@ -0,0 +1,25 @@
+module Main exposing (main)
+
+import Browser
+import Html exposing (Html)
+
+type alias Model = {}
+
+type alias Msg = Never
+
+update : Msg -> Model -> Model
+update msg _ = never msg
+
+view : Model -> Html Msg
+view _ = Html.h1 [] [ Html.text "Hello, World!" ]
+
+init : Model
+init = {}
+
+main : Program () Model Msg
+main =
+    Browser.sandbox
+        { init = init
+        , update = update
+        , view = view
+        }
