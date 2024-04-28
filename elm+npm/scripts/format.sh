9d9929afe6f6d58ce51f1b5177f3cb770a1dda75
dave4420 <dave4420@users.noreply.github.com>
2024-04-28 20:07:16 +0100

    Add basic “Hello world” Elm app (#1)

---
+++ elm+npm/scripts/format.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+default_base_branch=`
+    git symbolic-ref refs/remotes/origin/HEAD |
+    sed -nE 's_^([^/]+/){3}__p'
+`
+merge_base=`git merge-base ${base_commit:-$default_base_branch} HEAD`
+git diff --name-only $merge_base |
+sed -n 's_^elm+npm/__p' |
+grep '\.elm$' |
+xargs --no-run-if-empty elm-format --yes
