4234655a2730367c32f4f3609c8984bf9bb5e0f4
Dave Hinton <dave@4420.uk>
2024-05-06 08:38:51 +0100

    as part of `npm test`, ensure catalogue builds

--- elm+npm/scripts/test.sh
+++ elm+npm/scripts/test.sh
@@ -4,3 +4,4 @@ set -exuo pipefail
 
 # Make sure we at least typecheck modules that aren’t covered by tests
 elm make src/Main.elm --optimize --output=/dev/null
+elm make src/Catalogue.elm --output=/dev/null
