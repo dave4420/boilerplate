5400eaad33134cb67d56a1e3d39020a275da334e
Dave Hinton <dave@4420.uk>
2024-05-08 10:53:27 +0100

    ensure ts source files get autoformatted

--- elm+npm/scripts/format.sh
+++ elm+npm/scripts/format.sh
@@ -4,7 +4,13 @@ default_base_branch=`
     sed -nE 's_^([^/]+/){3}__p'
 `
 merge_base=`git merge-base ${base_commit:-$default_base_branch} HEAD`
+
 git diff --name-only $merge_base |
 sed -n 's_^elm+npm/__p' |
 grep '\.elm$' |
 xargs --no-run-if-empty elm-format --yes
+
+git diff --name-only $merge_base |
+sed -n 's_^elm+npm/__p' |
+grep '\.ts$' |
+xargs --no-run-if-empty prettier --write
