151cb551f759e4a3d06eab6bfa727871e1e1ef7f
Dave Hinton <dave@4420.uk>
2024-05-08 15:47:56 +0100

    add effects

--- elm+npm/src/Main.elm
+++ elm+npm/src/Main.elm
@@ -1,7 +1,14 @@
 port module Main exposing (main)
 
 import Browser
+import Op.Effect as Effect exposing (Effects)
 import View.Page.HomePage as HomePage
+
+
+port signIn : Effect.SignInParams -> Cmd m
+
+
+port signOut : () -> Cmd m
 
 
 port demandName : String -> Cmd m
@@ -10,12 +17,23 @@ port demandName : String -> Cmd m
 port receiveName : (String -> m) -> Sub m
 
 
+effectPorts : Effect.Ports Msg
+effectPorts =
+    { auth =
+        { signIn = signIn
+        , signOut = signOut ()
+        , demandName = demandName
+        }
+    }
+
+
 type alias Flags =
     ()
 
 
 type alias Model =
     { name : String
+    , effectConfig : Effect.Config
     }
 
 
@@ -23,12 +41,12 @@ type Msg
     = SetName String
 
 
-update : Msg -> Model -> ( Model, Cmd Msg )
+update : Msg -> Model -> ( Model, Effects Msg )
 update msg model =
     case msg of
         SetName newName ->
             ( { model | name = newName }
-            , Cmd.none
+            , []
             )
 
 
@@ -37,11 +55,12 @@ view model =
     HomePage.view { name = model.name }
 
 
-init : Flags -> ( Model, Cmd Msg )
+init : Flags -> ( Model, Effects Msg )
 init _ =
     ( { name = "World"
+      , effectConfig = {}
       }
-    , demandName "Bob"
+    , Effect.demandName "Bob"
     )
 
 
@@ -50,11 +69,16 @@ subscriptions _ =
     receiveName SetName
 
 
+publishEffects : ( Model, Effects Msg ) -> ( Model, Cmd Msg )
+publishEffects ( model, effects ) =
+    ( model, Effect.toCmd effectPorts model.effectConfig effects )
+
+
 main : Program Flags Model Msg
 main =
     Browser.document
-        { init = init
-        , update = update
+        { init = \flags -> init flags |> publishEffects
+        , update = \msg model -> update msg model |> publishEffects
         , view = view
         , subscriptions = subscriptions
         }
---
+++ elm+npm/src/Op/Effect.elm
@@ -0,0 +1,70 @@
+module Op.Effect exposing
+    ( Config
+    , Effects
+    , Ports
+    , SignInParams
+    , demandName
+    , signIn
+    , signOut
+    , toCmd
+    )
+
+import Op.Effect.Auth as Auth
+
+
+type Effect m
+    = Auth Auth.Effect
+
+
+type alias Effects m =
+    List (Effect m)
+
+
+type alias Ports m =
+    { auth : Auth.Ports m
+    }
+
+
+type alias Config =
+    {}
+
+
+toCmd : Ports m -> Config -> Effects m -> Cmd m
+toCmd ports _ effects =
+    let
+        f effect =
+            case effect of
+                Auth eff ->
+                    Auth.toCmd ports.auth eff
+    in
+    effects
+        |> List.map f
+        |> Cmd.batch
+
+
+
+-- Auth
+
+
+auth : Auth.Effect -> Effects m
+auth =
+    Auth >> List.singleton
+
+
+type alias SignInParams =
+    Auth.SignInParams
+
+
+signIn : SignInParams -> Effects m
+signIn params =
+    Auth.SignIn params |> auth
+
+
+signOut : Effects m
+signOut =
+    Auth.SignOut |> auth
+
+
+demandName : String -> Effects m
+demandName name =
+    Auth.DemandName name |> auth
