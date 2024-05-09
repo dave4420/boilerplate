fc9a15aadf924ef6bc803f90cb4e325ec6053ca2
Dave Hinton <dave@4420.uk>
2024-05-09 22:00:49 +0100

    add e-mail address and password fields

--- elm+npm/src/View/Page/SignInPage.elm
+++ elm+npm/src/View/Page/SignInPage.elm
@@ -2,6 +2,8 @@ module View.Page.SignInPage exposing (Field, Params, State(..), view)
 
 import Browser
 import Html exposing (..)
+import Html.Attributes as Att
+import Html.Events as Ev
 
 
 type alias Params m =
@@ -27,5 +29,24 @@ type State
 view : Params m -> Browser.Document m
 view params =
     { title = "Sign In"
-    , body = [ text "DAVE" ]
+    , body =
+        [ h1 [] [ text "Sign In" ]
+        , form []
+            [ viewField "E-mail address" "text" params.emailAddress
+            , viewField "Password" "password" params.emailAddress
+            ]
+        ]
     }
+
+
+viewField : String -> String -> Field m -> Html m
+viewField label type_ field =
+    p []
+        [ text <| label ++ " "
+        , input
+            [ Att.type_ type_
+            , Att.value field.value
+            , Ev.onInput field.onChange
+            ]
+            []
+        ]
