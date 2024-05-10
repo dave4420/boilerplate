c594c18631b38da891fb8bb3e0f9204036ab9b2f
Dave Hinton <dave@4420.uk>
2024-05-10 14:20:42 +0100

    Revert "extract types.ts WIP"
    This reverts commit 98ea1a5b290e6c7993b29fbac6b232bc6ab673b6.

--- elm+npm/ts/index.ts
+++ elm+npm/ts/index.ts
@@ -1,4 +1,24 @@
-import "./types";
+// types adapted from elm-ts-interop
+
+declare namespace Elm.Main {
+  function init(options: { node?: HTMLElement | null; flags: Flags }): App;
+}
+
+type Flags = null;
+
+type Cmd<Payload> = {
+  subscribe(callback: (payload: Payload) => void): void;
+  unsubscribe(callback: (payload: Payload) => void): void;
+};
+
+type Sub<Payload> = { send(payload: Payload): void };
+
+interface App {
+  ports: {
+    demandName: Cmd<string>;
+    receiveName: Sub<string>;
+  };
+}
 
 const app = Elm.Main.init({
   node: document.getElementById("app"),
