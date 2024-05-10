7456b7fc12ad7da8b1ec229255a87b984296ccda
Dave Hinton <dave@4420.uk>
2024-05-10 11:52:46 +0100

    extract types.ts WIP

--- elm+npm/ts/index.ts
+++ elm+npm/ts/index.ts
@@ -1,24 +1,4 @@
-// types adapted from elm-ts-interop
-
-declare namespace Elm.Main {
-  function init(options: { node?: HTMLElement | null; flags: Flags }): App;
-}
-
-type Flags = null;
-
-type Cmd<Payload> = {
-  subscribe(callback: (payload: Payload) => void): void;
-  unsubscribe(callback: (payload: Payload) => void): void;
-};
-
-type Sub<Payload> = { send(payload: Payload): void };
-
-interface App {
-  ports: {
-    demandName: Cmd<string>;
-    receiveName: Sub<string>;
-  };
-}
+import "./types";
 
 const app = Elm.Main.init({
   node: document.getElementById("app"),
