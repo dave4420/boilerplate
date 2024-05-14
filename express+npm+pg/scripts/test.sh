8c746da803deef650b42ab63cc66194836e58247
Dave Hinton <dave@4420.uk>
2024-05-14 14:01:34 +0100

    add openapi validation step

--- express+npm+pg/scripts/test.sh
+++ express+npm+pg/scripts/test.sh
@@ -4,5 +4,7 @@ IFS=$'\n\t'
 
 set -x
 
+docker run --rm -v "$PWD":/work:ro dshanley/vacuum lint -d *openapi.yaml
+
 # Make sure we at least typecheck modules that aren’t covered by tests
 tsc --noEmit --skipLibCheck
