8807353fb60782fad7a3dcba695a236ec4bfb06b
Dave Hinton <dave@4420.uk>
2024-05-16 09:35:06 +0100

    write tests for db methods

--- express+npm+pg/src/pg.test.ts
+++ express+npm+pg/src/pg.test.ts
@@ -1,6 +1,53 @@
-import { withPg } from "./pg";
+import { withPg, Thing } from "./pg";
 
 describe("things", () => {
-  it.todo("can save and retrieve a thing");
-  it.todo("can no longer retrieve a thing after deleting it");
+  it("can save and retrieve a thing", () =>
+    withPg(async (db) => {
+      // given
+      const thingId: Thing.Id = "1";
+      const thing: Thing = {
+        thingId,
+        name: "apple",
+        quantity: 1,
+      };
+      const expectedThings = [thing];
+
+      // when
+      await db.saveThing(thing);
+      const actualThings = await db.getThing(thingId);
+
+      // then
+      expect(actualThings).toEqual(expectedThings);
+    }));
+
+  it("can no longer retrieve a thing after deleting it", () =>
+    withPg(async (db) => {
+      // given
+      const thingId: Thing.Id = "2";
+      const thing: Thing = {
+        thingId,
+        name: "banana",
+        quantity: 7,
+      };
+
+      // when
+      await db.saveThing(thing);
+      await db.deleteThing(thingId);
+      const actualThings = await db.getThing(thingId);
+
+      // then
+      expect(actualThings).toEqual([]);
+    }));
+
+  it("can’t retrieve a thing that was never there", () =>
+    withPg(async (db) => {
+      // given
+      const thingId: Thing.Id = "3";
+
+      // when
+      const actualThings = await db.getThing(thingId);
+
+      // then
+      expect(actualThings).toEqual([]);
+    }));
 });
--- express+npm+pg/src/pg.ts
+++ express+npm+pg/src/pg.ts
@@ -1,9 +1,30 @@
+export type Thing = Readonly<{
+  thingId: Thing.Id;
+  name: string;
+  quantity: number;
+}>;
+
+export namespace Thing {
+  export type Id = string; // DAVE: make it branded
+  export const idOrNull = (raw: string): Id | null => raw; // DAVE: confirm it's a UUID
+}
+
 export interface Database {
   close(): Promise<void>;
+
+  saveThing(thing: Thing): Promise<void>;
+  getThing(thingId: Thing.Id): Promise<[Thing] | []>;
+  deleteThing(thingId: Thing.Id): Promise<void>;
 }
 
 export const pg = async (): Promise<Database> => {
-  throw Error("DAVE");
+  return {
+    close: async () => {},
+
+    saveThing: async (thing) => {},
+    getThing: async (thingId) => [],
+    deleteThing: async (thingId) => {},
+  };
 };
 
 export const withPg = async <R>(
