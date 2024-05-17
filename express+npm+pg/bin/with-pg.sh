c189b93bf35618fe86b9b1fac8a8d4a84689bab9
Dave Hinton <dave@4420.uk>
2024-05-17 07:51:17 +0100

    factor out start-pg, stop-pg scripts

--- express+npm+pg/bin/with-pg.sh
+++ express+npm+pg/bin/with-pg.sh
@@ -3,48 +3,12 @@ set -eu
 IFS=$'\n\t'
 # no -o pipefail because `docker logs` will write to a broken pipe
 
-source "$(dirname "$0")/pg.inc.sh"
+bin="$(dirname "$0")"
 
-trap 'docker kill pg' EXIT
+source "$bin/pg.inc.sh"
 
-docker run \
-    -p $PGPORT:5432 \
-    --tmpfs /var/lib/pg/data \
-    -e PGDATA=/var/lib/pg/data \
-    -e POSTGRES_USER=$PGUSER \
-    -e POSTGRES_PASSWORD=$PGPASSWORD \
-    -e POSTGRES_DB=$PGDATABASE \
-    --name pg \
-    --rm \
-    -d \
-    postgres:16.3-alpine3.19
+trap "$bin/stop-pg.sh" EXIT
 
-# https://hub.docker.com/_/postgres
-# Pick a version that matches what you have in production.
-# Alpine chosen purely because the image is smaller.
-
-# PG starts, stops, then starts again, so waiting for it to accept
-# connections isn't good enough — it might be about to shutdown before
-# starting again. We resort to log scraping.
-# https://github.com/docker-library/postgres/issues/146
-docker logs -f --since 1m pg 2>&1 |
-    (perl -e '
-        while (<>) {
-            last if /.*PostgreSQL init process complete; ready for start up.*/;
-        }
-        while (<>) {
-            last if /.*database system is ready to accept connections.*/;
-        }
-    ' && printf 'log something\n' >/dev/tcp/localhost/$PGPORT)
-
-docker run \
-    -v "$PWD"/migrations:/migrations \
-    --network host \
-    --name migrate \
-    --rm \
-    migrate/migrate \
-        -path=/migrations/ \
-        -database postgres://$PGUSER:$PGPASSWORD@localhost:$PGPORT/$PGDATABASE?sslmode=disable \
-        up
+"$bin/start-pg.sh"
 
 "${@:-$SHELL}"
