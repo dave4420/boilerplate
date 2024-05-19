c3d80bc30c6fc1368bf17597863f446e0c0f2d25
Dave Hinton <dave@4420.uk>
2024-05-19 09:35:53 +0100

    pass tests

--- express+npm+pg/migrations/0001_things.up.sql
+++ express+npm+pg/migrations/0001_things.up.sql
@@ -5,3 +5,15 @@ CREATE TABLE things (
     when_created    TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     when_updated    TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
+
+CREATE OR REPLACE FUNCTION update_when_updated() 
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.when_updated = now();
+    RETURN NEW; 
+END;
+$$ language 'plpgsql';
+
+CREATE TRIGGER update_things_when_updated
+BEFORE UPDATE ON things
+FOR EACH ROW EXECUTE PROCEDURE update_when_updated();
