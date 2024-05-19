9caf876f389222d8efc1221418b39f925a3e963b
Dave Hinton <dave@4420.uk>
2024-05-19 21:25:07 +0100

    make route take a uuid instead of an integer

--- express+npm+pg/src/my-api/index.ts
+++ express+npm+pg/src/my-api/index.ts
@@ -4,31 +4,10 @@ import { Request, Response } from "express";
 import asyncHandler from "express-async-handler";
 import * as schema from "../gen/openapi/my";
 
-// Ideally, the zod schema created by orval would do this for us
-// (convert the string in the request to the number we specfied in the
-// openapi schema), but alas no. There must be a better option out there
-// somewhere.
-const prepareThingParams = (input: any): any => {
-  if (input === null || typeof input !== "object") {
-    return input;
-  }
-  const { thingId, ...rest } = input;
-  if (typeof thingId !== "string") {
-    return input;
-  }
-  if (!/^[-+]?\d+$/.test(thingId)) {
-    return input;
-  }
-  return {
-    thingId: parseInt(thingId, 10),
-    ...rest,
-  };
-};
-
 const getThing = (log: Logger) =>
   asyncHandler(async (req: Request, res: Response) => {
     log.info("GET /stuff");
-    schema.getThingParams.parse(prepareThingParams(req.params));
+    schema.getThingParams.parse(req.params);
     throw Error("Not implemented");
   });
 
