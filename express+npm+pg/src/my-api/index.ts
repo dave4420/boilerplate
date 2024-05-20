ec42c3ba62b5ae8c3a68e472a298b789e05d2f69
Dave Hinton <dave@4420.uk>
2024-05-20 10:17:12 +0100

    first stab at implementing GET… doesn’t pass

--- express+npm+pg/src/my-api/index.ts
+++ express+npm+pg/src/my-api/index.ts
@@ -3,11 +3,40 @@ import express from "express";
 import { Request, Response } from "express";
 import asyncHandler from "express-async-handler";
 import * as schema from "../gen/openapi/my";
+import { pg } from "../pg";
+import { Thing } from "../domain";
+
+// DAVE: use connection pool instead of pg() directly
+
+const sendError = (status: number, message: string, res: Response) => {
+  // DAVE: persuade orval to generate zod for this
+  res.status(status).json({ message });
+};
 
 const getThing = (log: Logger) =>
   asyncHandler(async (req: Request, res: Response) => {
-    log.info("GET /stuff");
-    schema.getThingParams.parse(req.params);
+    const parsed = schema.getThingParams.safeParse(req.params);
+    if (!parsed.success) {
+      sendError(400, parsed.error.message, res);
+      return;
+    }
+    const thingId = Thing.idOrNull(parsed.data.thingId);
+    if (thingId === null) {
+      sendError(400, "thing not found", res);
+      return;
+    }
+    const db = await pg();
+    try {
+      const things = await db.getThing(thingId);
+      if (things.length === 0) {
+        sendError(404, "thing not found", res);
+        return;
+      }
+      const { thingId: _ignore, ...fields } = things[0];
+      res.json(schema.getThingResponse.parse(fields));
+    } finally {
+      await db.close();
+    }
     throw Error("Not implemented");
   });
 
