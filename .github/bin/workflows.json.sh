303bfb57bd9ca407d19567ffd42c90c57a1ac9da
Dave Hinton <dave@4420.uk>
2024-05-23 15:32:44 +0100

    filter out markdown files before deciding which subdirs have changed

--- .github/bin/workflows.json.sh
+++ .github/bin/workflows.json.sh
@@ -9,6 +9,7 @@ base="$(git merge-base "$target_branch" "$wip_branch")"
 
 git diff --name-status --no-renames "$base" "$wip_branch" |
     grep -F / |
+    grep --invert -E '\.md$' |
     sed -nE 's,^[^\t]+\t([^./][^/]*)/.*,\1,p' |
     sort -u |
     jq --raw-input --slurp --compact-output '
