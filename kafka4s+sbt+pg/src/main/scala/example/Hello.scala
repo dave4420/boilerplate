ed9852bf67caadd6bc7c1eb47cbea8d446a9cbc2
Dave Hinton <dave@4420.uk>
2024-05-27 08:44:41 +0100

    add debugging code

--- kafka4s+sbt+pg/src/main/scala/example/Hello.scala
+++ kafka4s+sbt+pg/src/main/scala/example/Hello.scala
@@ -4,7 +4,10 @@ import com.typesafe.scalalogging.Logger
 
 object Hello {
   def main(args: Array[String]): Unit = {
+    val j = new java.util.jar.JarFile(new java.io.File("target/scala-2.13/boilerplate-assembly-0.1.0-SNAPSHOT.jar"))
+    val mainClassName = j.getManifest().getMainAttributes().getValue("Main-Class")
     val logger = Logger("DAVE")
+    logger.info(mainClassName)
     logger.info(greeting)
   }
 
