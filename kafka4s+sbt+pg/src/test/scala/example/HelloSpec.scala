a943dce0816c113ba6e19b2ff5eaa0e3e8285adc
Dave Hinton <dave@4420.uk>
2024-06-10 19:37:58 +0100

    log from test case

--- kafka4s+sbt+pg/src/test/scala/example/HelloSpec.scala
+++ kafka4s+sbt+pg/src/test/scala/example/HelloSpec.scala
@@ -1,7 +1,11 @@
 package example
 
+import com.typesafe.scalalogging.Logger
+
 class HelloSpec extends munit.FunSuite {
   test("say hello") {
+    val logger = Logger("DAVE")
+    logger.info("DAVE")
     assertEquals(Hello.greeting, "hello")
   }
 }
