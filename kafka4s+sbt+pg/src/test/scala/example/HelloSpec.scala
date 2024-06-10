bc72f67dc1090aea3e0f028977cfddfeeca2b599
Dave Hinton <dave@4420.uk>
2024-06-10 19:50:20 +0100

    Revert "log from test case"
    This reverts commit a943dce0816c113ba6e19b2ff5eaa0e3e8285adc.

--- kafka4s+sbt+pg/src/test/scala/example/HelloSpec.scala
+++ kafka4s+sbt+pg/src/test/scala/example/HelloSpec.scala
@@ -1,11 +1,7 @@
 package example
 
-import com.typesafe.scalalogging.Logger
-
 class HelloSpec extends munit.FunSuite {
   test("say hello") {
-    val logger = Logger("DAVE")
-    logger.info("DAVE")
     assertEquals(Hello.greeting, "hello")
   }
 }
